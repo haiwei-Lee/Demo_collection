b87e807c7c41da63466466048a79de8aadfd4350
lihaiwei <lihaiwei@01zhuanche.com>
2019-05-23 16:22:09 +0800

    init project

---
+++ FunCollection/FunCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunCollection
+//
+//  Created by lee on 2019/5/23.
+//  Copyright © 2019 Personal Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunCollection/FunCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunCollection
+//
+//  Created by lee on 2019/5/23.
+//  Copyright © 2019 Personal Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
